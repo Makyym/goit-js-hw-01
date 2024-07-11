33f6114dfb3f37b43107536de5349081be1fcaac
Makyym <cosmonautmemory@gmail.com>
2024-07-11 20:27:54 +0300

    Test

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    
     return makeTransaction;
 };
 
